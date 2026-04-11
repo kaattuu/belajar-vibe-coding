6f9075c3e655fb2e123abbceb6439fa0c46192ab
kaattuu <donasitigasatu@gmail.com>
2026-04-12 01:23:08 +0800

    feat: implement user registration api

--- index.ts
+++ index.ts
@@ -1,8 +1,10 @@
 import { Elysia } from "elysia";
+import { usersRoute } from "./src/routes/users-route";
 
 const app = new Elysia()
   .get("/", () => "Hello Elysia from Bun!")
   .get("/health", () => ({ status: "ok" }))
+  .use(usersRoute)
   .listen(process.env.PORT || 3000);
 
 console.log(
